e5c1a78f24e60e2d5636d5eac4b6eab13193f20b
smarten <smarten.developer@gmail.com>
2020-12-01 20:54:18 -0800

    initilal

--- model/seat_down.sql
+++ model/seat_down.sql
@@ -1,12 +1,29 @@
-DROP DATABASE IF EXISTS db;
+DROP DATABASE IF EXISTS seat_db;
 
-CREATE DATABASE db;
+CREATE DATABASE seat_db;
 
-USE db;
+USE seat_db;
 
-CREATE TABLE tableName (
+CREATE TABLE chair (
     id INT AUTO_INCREMENT NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    deskId INT references desk(id),
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE company (
+    id INT AUTO_INCREMENT NOT NULL,
+    ownerId INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    deskCapacity INT NOT NULL,
+    address VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE desk (
+    id INT AUTO_INCREMENT NOT NULL,
+    companyId INT references company(id),
+    name VARCHAR(255) NOT NULL,
+    chairCapacity INT NOT NULL,
     PRIMARY KEY(id)
 );
