279e27414c317543fcc8732459b272fb5e042a5c
Bermond Jon Batistiana <berjondev@gmail.com>
2020-12-03 16:48:10 -0800

    started role routes and controller

--- model/deskQueries.js
+++ model/deskQueries.js
@@ -1,7 +1,7 @@
 const findAllDesksQuery = "SELECT * FROM desk;";
 const findDeskByIdQuery = "SELECT * FROM desk WHERE id = ?;";
 const insertDeskQuery =
-  "INSERT INTO desk (companyId, name, chairCapacity) VALUES (?, ?, ?);";
+  "INSERT INTO desk (id, companyId, name, chairCapacity) VALUES (?, ?, ?, ?);";
 const deleteDeskByIdQuery = "DELETE FROM desk WHERE ID = ?;";
 
 module.exports = {
---
+++ routes/apiRoutes/roleRoutes/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  getAllRolesApi,
+  getRoleByIdApi,
+  insertRoleApi,
+  deleteRoleByIdApi,
+} = require("../../../controllers/roleController");
+
+router.route("/")
+  .get(getAllRolesApi)
+  .post(insertRoleApi);
+router.route("/:roleId")
+  .get(getRoleByIdApi)
+  .delete(deleteRoleByIdApi);
+
+module.exports = router;
