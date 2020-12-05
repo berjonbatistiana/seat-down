95500190aa4853bb07db1635d472294e1cc7c2d0
smarten <smarten.developer@gmail.com>
2020-12-05 11:09:03 -0800

    n

--- client/src/pages/common/components/SeatingDetail.js
+++ client/src/pages/common/components/SeatingDetail.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function RecipeReviewCard() {
+export default function SeatingDetail() {
   const classes = useStyles();
 
   return (
