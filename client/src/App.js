6673057fd20f16658f3354293e10888515afcb9d
smarten <smarten.developer@gmail.com>
2020-12-05 11:06:06 -0800

    basic card

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { HashRouter as Router } from "react-router-dom";
-import SeatingDetail from "./pages/common/components/SeatingDetail";
 
 function App() {
-  return (
-    <Router>
-      <SeatingDetail />
-    </Router>
-  );
+  return <Router></Router>;
 }
 
 export default App;
