ca75393dcb0e348326a77e4497ac42a642c82a69
smarten <smarten.developer@gmail.com>
2020-12-07 19:49:04 -0800

    date stuff

--- client/src/pages/Viewer/ViewerViews/Dashboard.js
+++ client/src/pages/Viewer/ViewerViews/Dashboard.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  DatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
-import { Typography, Grid, Box , Paper} from "@material-ui/core";
+import React, { useState } from "react";
+import DateFnsUtils from "@date-io/date-fns";
+import { Badge } from "@material-ui/core";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { Typography, Grid, Box, Paper } from "@material-ui/core";
 
 export function Dashboard() {
   const [selectedDate, handleDateChange] = useState(new Date());
+  var d = new Date(2020, 11, 22);
+  var d2 = new Date(2020, 12, 19);
+  const days = [d, d2];
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -19,6 +20,17 @@ export function Dashboard() {
           openTo="date"
           value={selectedDate}
           onChange={handleDateChange}
+          renderDay={(day, selectedDate, isInCurrentMonth, dayComponent) => {
+            let badge = false;
+            days.forEach((item) => {
+              if (item.valueOf() === day.valueOf()) {
+                badge = true;
+              }
+            });
+            return (
+              <Badge badgeContent={badge ? 4 : undefined}>{dayComponent}</Badge>
+            );
+          }}
         />
       </Box>
     </MuiPickersUtilsProvider>
