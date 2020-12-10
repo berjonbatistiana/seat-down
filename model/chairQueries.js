e8046a371dc0d326bd9b0c02c56bb8f7e26ec6cd
Bermond Jon Batistiana <berjondev@gmail.com>
2020-12-09 19:51:40 -0800

    query added more information

--- model/chairQueries.js
+++ model/chairQueries.js
@@ -3,19 +3,27 @@ const findChairByIdQuery = "SELECT * FROM chairs WHERE id = ?;";
 const insertChairQuery = "INSERT INTO chairs (id, deskId, name) VALUES (?, ?, ?);";
 const deleteChairByIdQuery = "DELETE FROM chairs WHERE ID = ?;";
 const findAllAvailableChairsByCompanyQuery = `
-  SELECT chairs.name AS chairName, companies.name AS companyName, chairs.id AS chairId
+  SELECT
+    companies.name AS companyName,
+    buildings.id AS buildingId,
+    buildings.name AS buildingName,
+    floors.id AS floorId,
+    floors.name AS floorName,
+    chairs.id AS chairId,
+    chairs.name AS chairName
   FROM chairs
   JOIN desks
-  ON chairs.deskId = desks.id
+  ON desks.id = chairs.deskId
   JOIN floors
   ON floors.id = desks.floorId
   JOIN companies
   ON companies.id = floors.companyId
+  JOIN buildings
+  ON buildings.id = floors.buildingId
   WHERE chairs.id NOT IN (
     SELECT chairId
     FROM occupancy
-  )
-  AND companies.id = ?;
+  ) AND companies.id = ?;
  `;
 
 module.exports = {
