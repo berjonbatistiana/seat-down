50ea5b5f556c62d3b681e3f1fce04e8479f5fd07
smarten <smarten.developer@gmail.com>
2020-12-12 11:12:39 -0800

    temp

--- client/src/pages/Viewer/ViewerViews/Dashboard.js
+++ client/src/pages/Viewer/ViewerViews/Dashboard.js
@@ -3,51 +3,56 @@ import { Badge } from "@material-ui/core";
 import { DatePicker } from "@material-ui/pickers";
 import { Grid, Box } from "@material-ui/core";
 
-import { SeatingDetail } from '../../common/components';
+import { SeatingDetail } from "../../common/components";
+import { convertDate } from "../../../utils/tools";
 
 export function Dashboard() {
-  const [selectedDate, handleDateChange] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(convertDate(new Date()));
   const d = new Date(2020, 11, 22);
   const d2 = new Date(2020, 11, 18);
   const days = [d, d2];
 
+  const handleDateChange = (date) => {
+    setSelectedDate(convertDate(date));
+  };
+
   return (
-      <Grid container>
-        <Grid item>
-          <Box mt={3} ml={3} mr={3}>
-            <DatePicker
-              autoOk
-              orientation="landscape"
-              variant="static"
-              openTo="date"
-              value={selectedDate}
-              onChange={handleDateChange}
-              renderDay={(day, selectedDate, isInCurrentMonth, dayComponent) => {
-                let badge = false;
-                days.forEach((item) => {
-                  if (item.valueOf() === day.valueOf()) {
-                    badge = true;
-                  }
-                });
-                return (
-                  <Badge
-                    variant={badge ? "dot" : undefined}
-                    color={badge ? "secondary" : undefined}
-                    badgeContent={badge ? "" : undefined}
-                    overlap="circle"
-                  >
-                    {dayComponent}
-                  </Badge>
-                );
-              }}
-            />
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box mt={3} ml={3} mr={3}>
-            <SeatingDetail/>
-          </Box>
-        </Grid>
+    <Grid container>
+      <Grid item>
+        <Box mt={3} ml={3} mr={3}>
+          <DatePicker
+            autoOk
+            orientation="landscape"
+            variant="static"
+            openTo="date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            renderDay={(day, selectedDate, isInCurrentMonth, dayComponent) => {
+              let badge = false;
+              days.forEach((item) => {
+                if (item.valueOf() === day.valueOf()) {
+                  badge = true;
+                }
+              });
+              return (
+                <Badge
+                  variant={badge ? "dot" : undefined}
+                  color={badge ? "secondary" : undefined}
+                  badgeContent={badge ? "" : undefined}
+                  overlap="circle"
+                >
+                  {dayComponent}
+                </Badge>
+              );
+            }}
+          />
+        </Box>
       </Grid>
+      <Grid item>
+        <Box mt={3} ml={3} mr={3}>
+          <SeatingDetail />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
