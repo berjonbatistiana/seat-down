51c247dcf6e178af4f9e05448e288639d3673159
Bermond Jon Batistiana <berjondev@gmail.com>
2020-12-19 10:38:51 -0800

    performance fix

--- client/src/pages/Viewer/ViewerViews/Reservation.js
+++ client/src/pages/Viewer/ViewerViews/Reservation.js
@@ -10,68 +10,9 @@ import {
   reserveSeat,
 } from "../../../utils";
 import {convertDate, getLocalDate} from "../../../utils/tools";
-
-
-import MaterialTable, {MTableToolbar} from "material-table";
-import EventSeatIcon from "@material-ui/icons/EventSeat";
+import {SeatingTable} from "../../common/components/"
 
 import {DatePicker} from "../../common";
-import {
-  AddBox,
-  ArrowDownward,
-  Check,
-  ChevronLeft,
-  ChevronRight,
-  Clear,
-  DeleteOutline,
-  Edit,
-  FilterList,
-  FirstPage,
-  LastPage,
-  Remove,
-  SaveAlt,
-  Search,
-  ViewColumn,
-} from "@material-ui/icons";
-
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref}/>
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref}/>
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>),
-};
-
-const columns = [
-  {
-    title: "Building",
-    field: "buildingName",
-  },
-  {
-    title: "Floor",
-    field: "floorName",
-  },
-  {
-    title: "Chair",
-    field: "chairName",
-  },
-];
 
 export const Reservation = () => {
   const [userId, setUserId] = React.useState('');
@@ -126,7 +67,7 @@ export const Reservation = () => {
       console.error(e);
     });
   };
-
+  
   const renderTableTitle = () => {
     return (
       <Box pt={3}>
@@ -157,79 +98,18 @@ export const Reservation = () => {
       </Box>
     );
   };
+
+  
   return (
     <form>
       <Box m={3} component={Paper} variant="outlined">
-        <MaterialTable
-          pr={3}
-          icons={tableIcons}
-          title={renderTableTitle()}
-          columns={columns}
-          data={availableSeats}
-          actions={
-            !hasSeat
-              ? [
-                {
-                  icon: EventSeatIcon,
-                  tooltip: "Reserve Seat",
-                  onClick: handleReserveSeat,
-                },
-              ]
-              : []
-          }
-          options={{
-            doubleHorizontalScroll: true,
-            detailPanelType: "single",
-            actionsColumnIndex: -1,
-            searchFieldAlignment: "right",
-            actionsCellStyle: {
-              padding: "25px",
-            },
-          }}
-          localization={{
-            header: {
-              actions: "Reserve",
-            },
-            body: {
-              emptyDataSourceMessage: areSeatsLoading ? (
-                <Typography variant="h6">
-                  Loading seats, please wait.
-                </Typography>
-              ) : !hasSeat ? (
-                <Typography variant="h6">
-                  Sorry, no seats are available for this day.
-                </Typography>
-              ) : (
-                <Typography variant="h6">
-                  You already have a seat.
-                  <Button
-                    variant="contained"
-                    style={{
-                      marginLeft: 5,
-                      marginRight: 5,
-                      color: "white",
-                      backgroundColor: "#fd8369",
-                      borderRadius: 25,
-                      "&:hover": {
-                        backgroundColor: "#fd8369",
-                      },
-                    }}
-                    onClick={handleRemoveSeat}
-                  >
-                    Click here
-                  </Button>
-                  to remove previous reservation.
-                </Typography>
-              ),
-            },
-          }}
-          components={{
-            Toolbar: (props) => (
-              <div style={{paddingRight: "40px"}}>
-                <MTableToolbar {...props} />
-              </div>
-            ),
-          }}
+        <SeatingTable
+          hasSeat={hasSeat}
+          areSeatsLoading={areSeatsLoading}
+          renderTableTitle={renderTableTitle}
+          handleRemoveSeat={handleRemoveSeat}
+          handleReserveSeat={handleReserveSeat}
+          availableSeats={availableSeats}
         />
       </Box>
     </form>
--- client/src/pages/common/components/index.js
+++ client/src/pages/common/components/index.js
@@ -8,3 +8,4 @@ export * from "./SeatingDetail";
 export * from "./UserNavbar";
 export * from "./AdderModal";
 export * from "./Footer";
+export * from "./SeatingTable";
