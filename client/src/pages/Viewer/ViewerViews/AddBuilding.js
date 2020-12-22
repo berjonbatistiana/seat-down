00120cdc257a13ac7cf6eff2b1426c6458c529b2
smarten <smarten.developer@gmail.com>
2020-12-21 19:15:23 -0800

    sdaf

--- client/src/pages/Viewer/ViewerViews/AddBuilding.js
+++ client/src/pages/Viewer/ViewerViews/AddBuilding.js
@@ -228,7 +228,16 @@ export const AddBuilding = () => {
                 id="numFloors"
                 label="Number of Floors"
                 type="number"
-                InputProps={{ inputProps: { min: 1, max: 9 } }}
+                defaultValue={1}
+                InputProps={{
+                  inputProps: {
+                    min: 1,
+                    max: 9,
+                    onKeyDown: (event) => {
+                      event.preventDefault();
+                    },
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={7}>
@@ -248,7 +257,16 @@ export const AddBuilding = () => {
                 id="tablesPerFloor"
                 label="Tables per Floor"
                 type="number"
-                InputProps={{ inputProps: { min: 1, max: 9 } }}
+                defaultValue={1}
+                InputProps={{
+                  inputProps: {
+                    min: 1,
+                    max: 9,
+                    onKeyDown: (event) => {
+                      event.preventDefault();
+                    },
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={7}>
@@ -267,7 +285,16 @@ export const AddBuilding = () => {
                 fullWidth
                 id="chairsPerTable"
                 label="Chairs per Table"
-                InputProps={{ inputProps: { min: 1, max: 9 } }}
+                defaultValue={1}
+                InputProps={{
+                  inputProps: {
+                    min: 1,
+                    max: 9,
+                    onKeyDown: (event) => {
+                      event.preventDefault();
+                    },
+                  },
+                }}
                 type="number"
               />
             </Grid>
